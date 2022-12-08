34b01c605e59af86d26fb9572f50b2115ed4964a
Runelf <runelf.bushi@gmail.com>
2022-12-08 17:34:26 +0300

    BNA update socket project

--- src/test/java/socket/Client.java
+++ src/test/java/socket/Client.java
@@ -1,2 +1,52 @@
-package socket;public class Client {
+package socket;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
+
+public class Client extends WebSocketClient {
+    private final SocketContext context;
+    private Date openedTime;
+
+    public Client(SocketContext context) throws URISyntaxException {
+        super(new URI(context.getURI()));
+        this.context = context;
+    }
+
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        openedTime = new Date();
+        System.out.println("Opened Connection " + context.getURI());
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("Received new message " + message);
+        context.getMessageList().add(message);
+        if(message.equals(context.getExpectedMessage())){
+            closeConnection(1000, "Received expected message");
+        }
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("Close socket with code "+ code + ", reason is " + reason );
+        context.setStatusCode(code);
+    }
+
+    @Override
+    public void onError(Exception ex) {
+
+    }
+
+    public int getAliveTime(){
+        Date closedDate = new Date();
+        int timeIneSeconds = (int) (closedDate.getTime() - openedTime.getTime()) / 1000;
+        context.setTimeTaken(timeIneSeconds);
+        return timeIneSeconds;
+    }
 }
--- src/test/java/socket/WebClient.java
+++ src/test/java/socket/WebClient.java
@@ -1,2 +1,50 @@
-package socket;public class WebClient {
+package socket;
+
+import java.net.URISyntaxException;
+import java.util.Map;
+
+public class WebClient {
+    private Client client;
+
+    private WebClient() {
+    }
+
+    public static WebClient getInstance() {
+        return new WebClient();
+    }
+
+    public void connectToSocket(SocketContext context) {
+        boolean isBodySent = false;
+        try {
+            client = new Client(context);
+            if (!context.getRequestHeaders().isEmpty()) {
+                final Map<String, String> requestHeaderParams = context.getRequestHeaders();
+                requestHeaderParams.forEach((key, value) -> {
+                    client.addHeader(key, value);
+                });
+            }
+
+            client.connectBlocking();
+            while (!client.isClosed()) {
+                if (client.getAliveTime() >= context.getTimeOut()) {
+                    client.close(1006, "Time out");
+                }
+                if (context.getRunnable() != null) {
+                    context.getRunnable().run();
+                }
+                if (context.getBody() != null && !isBodySent) {
+                    client.send(context.getBody());
+                    isBodySent = true;
+                }
+                if (context.getExpectedMessage() != null) {
+                    client.onMessage(context.getExpectedMessage());
+                    return;
+                }
+
+            }
+
+        } catch (URISyntaxException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
